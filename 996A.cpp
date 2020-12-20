ccc22f78ec8502c120059ab4649d80e0991caa6b
abraham-tsang <abraham.tsang@gmail.com>
2020-12-19 20:45:10 -0500

    finished 996A

--- 996A.cpp
+++ 996A.cpp
@@ -5,6 +5,11 @@
 int f996A(int input){
     std::vector<int> coins = {1, 5, 10, 20, 100};
     std::vector<int> dp;
+    int additional = 0;
+    if(input != input%100){
+	additional = (input - input%100)/100;
+	input = input%100;
+    }
     for(int i = 0; i < input+1; i++){
 	dp.push_back(-1);
     }
@@ -29,50 +34,85 @@ int f996A(int input){
 	    dp[i] = min + 1;
 	}
     }
-    return dp[input];
+    return dp[input] + additional;
 }
 */
+
+/*
+int f996A_recursive(int input, std::vector<int> & coins, std::vector<int> & dp){
+    //Base
+    if(input == 0){
+	return 0;
+    }
+    //Recursive
+    else{
+	int original = input;
+	int min = 99999;
+	int temp;
+	for(int i = 0; i < coins.size(); i++){
+	    input = original;
+	    if(input - coins[i] >= 0){
+		temp = f996A_recursive(input - coins[i], coins, dp);
+		if(min > temp){
+		    min = temp;
+		}
+	    }
+	}
+	return 1 + min;
+    }
+}
+*/
+
+int f996A_recursive(int input, std::vector<int> & coins, std::vector<int> & dp){
+    //Base
+    if(input == 0){
+	return 0;
+    }
+    //Recursive
+    else{
+	if(dp[input] == -1){
+	    int original = input;
+	    int min = 99999;
+	    int temp;
+	    for(int i = 0; i < coins.size(); i++){
+	        input = original;
+	        if(input - coins[i] >= 0){
+		    if(dp[input - coins[i]] != -1){
+			temp = dp[input - coins[i]];
+		    }
+		    else{
+		        temp = f996A_recursive(input - coins[i], coins, dp);
+		    }
+		    if(min > temp){
+		        min = temp;
+		    }
+	        }
+	    }
+	    dp[input] = 1 + min;
+	}
+	return dp[input];
+    }
+}
 
 int f996A(int input){
     std::vector<int> coins = {1, 5, 10, 20, 100};
     std::vector<int> dp;
-    int size = input;
-    if(input > 101){
-	size = 101;
+    int additional = 0;
+    if(input != input%100){
+	additional = (input - input%100)/100;
+	input = input%100;
     }
-    for(int i = 0; i < size; i++){
+    for(int i = 0; i < input+1; i++){
 	dp.push_back(-1);
     }
     dp[0] = 0;
-    std::vector<int> temp;
-    int min;
-    int it = 0;
-    for(int i = 0; i < input+1; i++){ // change i to fit into 101
-	temp = {};
-	for(int j = 0; j < 5; j++){
-	    if((i-coins[j]) % 101 < 0){
-		break;
-	    }
-	    temp.push_back(dp[(i-coins[j]) % 101]);
-	}
-	min = 99999;
-        for(int j = 0; j < temp.size(); j++){
-	    if(min > temp[j]){
-		min = temp[j];
-	    }
-	}
-	if(i % 101 == 0 && it == 0){
-	    it++;
-	}
-	else{
-	    dp[i % 101] = min + 1;
-	}
-    }
-    return dp[input%101];
+    return f996A_recursive(input, coins, dp) + additional;
 }
 
-/*
+
 int main(){
-    std::cout << f996A(1000000000);
+    int input;
+    std::cin >> input;
+    std::cout << f996A(input);
 }
-*/
+
--- 996A.h
+++ 996A.h
@@ -1,3 +1,5 @@
 #include <vector>
 
+int f996A_recursive(int input);
+
 int f996A(int input);
